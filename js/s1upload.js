59f570ff3e4de7d1fbfcd9f58d8095bc9dca5ff2
youngjae <elite1111cyj@gmail.com>
2020-11-29 23:44:23 +0900

    design adpation

--- js/s1upload.js
+++ js/s1upload.js
@@ -9,6 +9,7 @@ function upbtnclicked(){
     console.log("btn clicked");
     var f_name=document.getElementById("f_name").value
     var chat_log=document.getElementById("message").value
+    f_name=f_name.replaceAll(/%20/g, "_");
 
     if(f_name.length>4 && chat_log.length>5){
       console.log(f_name, chat_log);
