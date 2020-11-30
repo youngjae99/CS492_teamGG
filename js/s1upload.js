5422901ecc0df8bf7a3467333d9625c3eb9f525c
zhyeong <z.hyeong@kaist.ac.kr>
2020-11-30 12:59:45 +0900

    testing front-back joint

--- js/s1upload.js
+++ js/s1upload.js
@@ -1,3 +1,6 @@
+
+
+
 $(document).ready(function () {
   var uploadbtn = document.getElementById("upbtn");
   uploadbtn.onkeypress = function () {
@@ -8,14 +11,111 @@ $(document).ready(function () {
 function upbtnclicked(){
     console.log("btn clicked");
     var f_name=document.getElementById("f_name").value
+    var f_data=document.getElementById("f_data").value
     var chat_log=document.getElementById("message").value
     f_name=f_name.replaceAll(/%20/g, "_");
 
     if(f_name.length>1 && chat_log.length>5){
       console.log(f_name, chat_log);
-      location.href = "./s2control.html?name=" + f_name+"&date="+1129;
+      analyzeSentimentInText(f_name, f_data,'Neutral', chat_log)
+      location.href = "./s2control.html?name=" + f_name+"&date="+f_data;
     }
     else{
       alert("Your input is too short!");
     }
 }
+
+
+async function analyzeSentimentInText(name, date, user_input, text) {
+  process.env['GOOGLE_APPLICATION_CREDENTIALS'] = './gg_certif.json'
+  // [START language_sentiment_gcs]
+  // Imports the Google Cloud client library
+  const language = require('@google-cloud/language');
+
+  // Creates a client
+  const client = new language.LanguageServiceClient();
+
+  // Prepares a document, representing a text file in Cloud Storage
+  const document = {
+    content: text,
+    type: 'PLAIN_TEXT',
+  }; 
+
+  // Detects the sentiment of the document
+  const [result] = await client.analyzeSentiment({document});
+
+  const sentiment = result.documentSentiment;
+  // console.log('Document sentiment:');
+  // console.log(`  Score: ${sentiment.score}`);
+  // console.log(`  Magnitude: ${sentiment.magnitude}`);
+
+  const sentences = result.sentences;
+  // sentences.forEach(sentence => {
+  //   console.log(`Sentence: ${sentence.text.content}`);
+  //   console.log(`  Score: ${sentence.sentiment.score}`);
+  //   console.log(`  Magnitude: ${sentence.sentiment.magnitude}`);
+  // });
+
+  var doc_score = sentiment.score
+  if (doc_score > 0.3){
+    var posneg = "positive"
+  }else if (doc_score < -0.3){
+    var posneg = "negative"
+  }else{
+    var posneg = "neutral"
+  }
+
+  var results = []
+
+  for (item of sentences){
+    value = item.sentiment.score
+    instance = item.text.content
+    results[results.length] = {'value':value, 'sentence':instance}
+  };
+
+  results.sort(function (a,b) {
+    if (a.value > b.value) {
+      return 1;
+    }
+    if (a.value < b.value) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  })
+  
+  const negment_ = results.slice(0,3);
+  const negment = negment_.map(({sentence})=>sentence)
+  const posment_ = results.slice(-3);
+  const posment = posment_.map(({sentence})=>sentence)
+
+  // console.log(posment)
+
+  // update to Firebase
+  var firebase = require("firebase/app");
+  require('firebase/database');
+
+  var firebaseConfig = 
+  {apiKey: "AIzaSyCg2H_5AnifrBDQZ-gY82UtZYVwkfVLZPg",
+    authDomain: "hai-gg-f140f.firebaseapp.com",
+    databaseURL: "https://hai-gg-f140f.firebaseio.com",
+    projectId: "hai-gg-f140f",
+    storageBucket: "hai-gg-f140f.appspot.com",
+    messagingSenderId: "108882535634",
+    appId: "1:108882535634:web:4a8200edf49c777a8eb90b",
+    measurementId: "G-20V6VJBDGD" }; // Initialize 
+
+  firebase.initializeApp(firebaseConfig); 
+  var database=firebase.database();
+
+  database.ref(`${name}/${date}`).update({
+    "human_posneg": user_input,
+    "negment": negment,
+    "point": doc_score,
+    "posment": posment,
+    "posneg":posneg
+  });
+
+  // [END language_sentiment_gcs]
+}
+
