03f0cb87298ae5496438667bbeffee49365433ad
Soyoung Yang <dudrrm1@gmail.com>
2020-12-22 00:16:53 +0900

    add example sentences

--- js/s0_customize_0_neg.js
+++ js/s0_customize_0_neg.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
 negative_thresholds={}
 negative_thresholds_sequence=['-0.1','-0.2','-0.3','-0.4','-0.5','-0.6','-0.7','-0.8','-0.89']
 negative_thresholds['-0.89']=["I don't like you", "I hate you", "Move your ass","Son of a bitch"]
-negative_thresholds['-0.8']=["What the hell","I don't want to talk to you anymore","You smell bad"]
-negative_thresholds['-0.7']=["I can't understand everything you did."]
-negative_thresholds['-0.6']=["Fuck off","I don't care what you think","You've got no second chance","You're being recalled"]
-negative_thresholds['-0.5']=["Honestly, I don't know where this is going.","There's no second chance."]
-negative_thresholds['-0.4']=["Can't you understand such small thing?","You think you could do that."]
-negative_thresholds['-0.3']=['Bitch,shut up.']
-negative_thresholds['-0.2']=['And then what.']
-negative_thresholds['-0.1']=['I will hunt you.','I mean do this work according to the situations.']
+negative_thresholds['-0.8']=["What the hell","I don't want to talk to you anymore","You smell bad", 'She is horrible.', 'I couldn\'t face the embarrassment.', 'How simply awful.']
+negative_thresholds['-0.7']=["I can't understand everything you did.", 'That\'s not quite what I mean', 'It\'s not any of my business.', 'No wonder you were terrified!' ]
+negative_thresholds['-0.6']=["Fuck off","I don't care what you think","You've got no second chance","You're being recalled", 'I cannot get out.', 'What on earth\'s going on?']
+negative_thresholds['-0.5']=["Honestly, I don't know where this is going.","There's no second chance.", 'It was tough day.', 'You can dismiss him.']
+negative_thresholds['-0.4']=["Can't you understand such small thing?","You think you could do that.", 'She wouldn\'t cost you a penny.', 'So I scratch my head.']
+negative_thresholds['-0.3']=['Bitch,shut up.', 'It\'s too general.', 'It is some who is dainty with his eating.']
+negative_thresholds['-0.2']=['And then what.', 'We\'ll have to run.', 'You mean you\'ve looked at the pictures?', 'Please tell it.']
+negative_thresholds['-0.1']=['I will hunt you.','I mean do this work according to the situations.', 'Did she beat you?', "Waht you\'ve done?"]
 // the content needs to be changed!
 
 
--- js/s0_customize_1_pos.js
+++ js/s0_customize_1_pos.js
@@ -4,15 +4,15 @@ $(document).ready(function () {
 
 positive_thresholds={}
 positive_thresholds_sequence=['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.89']
-positive_thresholds['0.89']=["I don't like you", "I hate you", "Move your ass","Son of a bitch"]
-positive_thresholds['0.8']=["What the hell","I don't want to talk to you anymore","You smell bad"]
-positive_thresholds['0.7']=["I can't understand everything you did."]
-positive_thresholds['0.6']=["I saw you in my dreams.","I don't care what you think","You've got no second chance","You're being recalled"] 
-positive_thresholds['0.5']=["Honestly, I don't know where this is going.","There's no second chance."]
-positive_thresholds['0.4']=["Can't you understand such small thing?","You think you could do that."]
-positive_thresholds['0.3']=['Bitch,shut up.']
-positive_thresholds['0.2']=['And then what.']
-positive_thresholds['0.1']=['I will hunt you.','I mean do this work according to the situations.']
+positive_thresholds['0.89']=["What a lovely day!", "That coffee was the best one ever.", "Keep going.", "I\'ll meet my lovely boyfriend.", "I like warm hug.", "Oh, well done you!"]
+positive_thresholds['0.8']=["It was a cozy day.", "I\'m good.", "She read the sentence without any hesitation at all.", "I\'m glad it\'s happened.", "I\'m willing."]
+positive_thresholds['0.7']=["I play the playlist all day.", "It\'s coming, treasure.", "She must be a brilliant teacher.", "Go ahead."]
+positive_thresholds['0.6']=["I saw you in my dreams.", "Rock n roll", "But others were lovely.", "You can take more than one if you wish.", "It is easy.", "It\'s a famous one."] 
+positive_thresholds['0.5']=["It\'s up to you.", "I\'m not missing that plane!", "I think so, yes.", "Life is a music."]
+positive_thresholds['0.4']=["Well, I guess that was true.",]
+positive_thresholds['0.3']=["It\'s like music.", "I\'m sure I can manage.", "Come here.", "That\'s quite enough from you.", "How right you are."]
+positive_thresholds['0.2']=['How right you are.', 'You can help me make the tea.', 'I was free at last.', 'Why, thank you', "But there is some kind of magic in you somewhere."]
+positive_thresholds['0.1']=['You can do it.', 'Let\'s have another cup of tea.', 'Of course you may try it.', 'I\'ve got it.']
 
 function getParam(sname) {
   var params = location.search.substr(location.search.indexOf("?") + 1);
